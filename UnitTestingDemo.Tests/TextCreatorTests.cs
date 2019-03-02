6a34b5e5d9a414307c0f98b787b00884573dd7a1
Wojciech Rak <rak.wojtek@wp.pl>
2019-03-02 21:57:17 +0100

    FirstUse

--- UnitTestingDemo.Tests/TextCreatorTests.cs
+++ UnitTestingDemo.Tests/TextCreatorTests.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
 
 namespace UnitTestingDemo.Tests
 {
-    class TextCreatorTests
+    [TestFixture]
+    public class TextCreatorTests
     {
+        [Test]
+        public void ExportText_ProperUse_ReturnCorrectStringAndClearSB()
+        {
+            //Arrange
+            var textCreator = new TextCreator();
+            textCreator.AddTitle("Title");
+            textCreator.AddDescription("Description");
+
+            //Act
+            var resultText = textCreator.ExportText();
+            var expectedText = "Title\r\nDescription\r\n";
+            
+            var resultClean = textCreator.ExportText();
+            var expectedCleanedText = "";
+            
+            //Assert
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(expectedText, resultText);
+                Assert.AreEqual(expectedCleanedText, resultClean);
+            });
+            
+        }
     }
 }
--- UnitTestingDemo/TextCreator.cs
+++ UnitTestingDemo/TextCreator.cs
@@ -6,5 +6,23 @@ namespace UnitTestingDemo
 {
     public class TextCreator
     {
+        private readonly StringBuilder _sb = new StringBuilder();
+
+        public void AddTitle(string title)
+        {
+            _sb.AppendLine(title);
+        }
+
+        public void AddDescription(string description)
+        {
+            _sb.AppendLine(description);
+        }
+
+        public string ExportText()
+        {
+            var exportedText = _sb.ToString();
+            _sb.Clear();
+            return exportedText;
+        }
     }
 }
